73e176e192347e6336218d15f10c79ef014b8838
michellehand <michellehand@outlook.com>
2014-04-07 22:24:03 -0500

    add, remove, and clear input jquery

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-	$('#button').click(function() {
-		var item = ($("#itemEntry").val());
-		$(".additem").append('<li>+item+</li>');
-		});
+	//add items
+	$('#addButton').click(function(event) {
+		event.preventDefault();
+		var item = $("#itemEntry").val();
+		$("#items").append('<li>' + item + '<img src="images/trash.png"></li>');
+		$("input").val('');
 	});
+
+	//remove items
+	$(document).on('click', 'img', function() {
+		$(this).parent().remove();
+	});
+
+});
