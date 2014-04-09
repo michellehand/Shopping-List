36af51f3612344f3339a94f0316a8caab87d3176
michellehand <michellehand@outlook.com>
2014-04-09 13:04:54 -0500

    add strikethrough, reset button

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ $(document).ready(function() {
 		event.preventDefault();
 		var item = $("#itemEntry").val();
 		$("#items").append('<li>' + item + '<img src="images/trash.png"></li>');
+		//clears input field
 		$("input").val('');
 	});
-
 	//remove items
 	$(document).on('click', 'img', function() {
 		$(this).parent().remove();
 	});
-
+	//strikethrough item
+	$(document).on('click', 'li', function() {
+		$(this).toggleClass('strike');
+	});
+	//clear list
+	$('#reset').click(function(event) {
+		event.preventDefault();
+		$('ul').empty(); 
+	});
 });
